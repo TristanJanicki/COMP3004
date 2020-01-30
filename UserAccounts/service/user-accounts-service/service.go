b2e9736615d73ae37e82f1c304cfb2f319858a7d
Tristan Janicki <tristan.janicki@gmail.com>
2020-01-30 17:58:41 -0500

    Update service.go

--- UserAccounts/service/user-accounts-service/service.go
+++ UserAccounts/service/user-accounts-service/service.go
@@ -51,20 +51,20 @@ func (s *UserAccountsService) Init(config httpservice.ServiceConfig, listenAddre
 	s.awsSession, s.awsConfig = NewAwsSession()
 	s.cognitoHandler = cognito.NewAwsCognitoHandler(s.awsSession, s.awsConfig, &userPoolId, &appClientId)
 
-	// Initialize DB manager
-	s.sqlDbManager, err = mysql.New(config)
-	if err != nil {
-		log.WithError(err).Error("Failed to initialize database connection")
-		return errors.Wrapf(err, "Failed to initialize database connection")
-	}
+	// // Initialize DB manager
+	// s.sqlDbManager, err = mysql.New(config)
+	// if err != nil {
+	// 	log.WithError(err).Error("Failed to initialize database connection")
+	// 	return errors.Wrapf(err, "Failed to initialize database connection")
+	// }
 
-	// Initialize handlers
-	s.authHandler = handlers.NewAuthenticationHandler(s.sqlDbManager, s.cognitoHandler)
-	s.userAccountsHandler, err = handlers.NewUserAccountsHandler(s.sqlDbManager, s.cognitoHandler)
-	if err != nil {
-		log.WithError(err).Error("Failed to initialize user accounts handler")
-		return errors.Wrapf(err, "Failed to initialize user accounts handler")
-	}
+	// // Initialize handlers
+	// s.authHandler = handlers.NewAuthenticationHandler(s.sqlDbManager, s.cognitoHandler)
+	// s.userAccountsHandler, err = handlers.NewUserAccountsHandler(s.sqlDbManager, s.cognitoHandler)
+	// if err != nil {
+	// 	log.WithError(err).Error("Failed to initialize user accounts handler")
+	// 	return errors.Wrapf(err, "Failed to initialize user accounts handler")
+	// }
 
 	// Initialize API
 	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
@@ -74,27 +74,27 @@ func (s *UserAccountsService) Init(config httpservice.ServiceConfig, listenAddre
 	}
 	api := operations.NewSwaggerRestAPI(swaggerSpec)
 
-	// Link handlers
-	api.SignUpHandler = operations.SignUpHandlerFunc(s.authHandler.SignUp)
-	api.RequestPasswordRecoveryHandler = operations.RequestPasswordRecoveryHandlerFunc(s.authHandler.RequestPasswordRecovery)
-	api.CompletePasswordRecoveryHandler = operations.CompletePasswordRecoveryHandlerFunc(s.authHandler.CompletePasswordRecovery)
-	api.ChangePasswordHandler = operations.ChangePasswordHandlerFunc(s.authHandler.ChangePassword)
-	api.SignInHandler = operations.SignInHandlerFunc(s.authHandler.SignIn)
-	api.SignOutHandler = operations.SignOutHandlerFunc(s.authHandler.SignOut)
-	api.CompleteAuthChallengeHandler = operations.CompleteAuthChallengeHandlerFunc(s.authHandler.CompletePasswordChallenge)
-	api.RefreshTokensHandler = operations.RefreshTokensHandlerFunc(s.authHandler.RefreshTokens)
-	api.VerifyJwtHandler = operations.VerifyJwtHandlerFunc(s.authHandler.VerifyJwt)
+	// // Link handlers
+	// api.SignUpHandler = operations.SignUpHandlerFunc(s.authHandler.SignUp)
+	// api.RequestPasswordRecoveryHandler = operations.RequestPasswordRecoveryHandlerFunc(s.authHandler.RequestPasswordRecovery)
+	// api.CompletePasswordRecoveryHandler = operations.CompletePasswordRecoveryHandlerFunc(s.authHandler.CompletePasswordRecovery)
+	// api.ChangePasswordHandler = operations.ChangePasswordHandlerFunc(s.authHandler.ChangePassword)
+	// api.SignInHandler = operations.SignInHandlerFunc(s.authHandler.SignIn)
+	// api.SignOutHandler = operations.SignOutHandlerFunc(s.authHandler.SignOut)
+	// api.CompleteAuthChallengeHandler = operations.CompleteAuthChallengeHandlerFunc(s.authHandler.CompletePasswordChallenge)
+	// api.RefreshTokensHandler = operations.RefreshTokensHandlerFunc(s.authHandler.RefreshTokens)
+	// api.VerifyJwtHandler = operations.VerifyJwtHandlerFunc(s.authHandler.VerifyJwt)
 
-	api.CreateUserAccountHandler = operations.CreateUserAccountHandlerFunc(s.userAccountsHandler.CreateUserAccount)
-	api.UpdateUserAccountHandler = operations.UpdateUserAccountHandlerFunc(s.userAccountsHandler.UpdateUserAccount)
-	api.GetUserAccountHandler = operations.GetUserAccountHandlerFunc(s.userAccountsHandler.GetUserAccount)
-	api.GetAllUserAccountsHandler = operations.GetAllUserAccountsHandlerFunc(s.userAccountsHandler.GetAllUserAccounts)
-	api.DeleteUserAccountHandler = operations.DeleteUserAccountHandlerFunc(s.userAccountsHandler.DeleteUserAccount)
+	// api.CreateUserAccountHandler = operations.CreateUserAccountHandlerFunc(s.userAccountsHandler.CreateUserAccount)
+	// api.UpdateUserAccountHandler = operations.UpdateUserAccountHandlerFunc(s.userAccountsHandler.UpdateUserAccount)
+	// api.GetUserAccountHandler = operations.GetUserAccountHandlerFunc(s.userAccountsHandler.GetUserAccount)
+	// api.GetAllUserAccountsHandler = operations.GetAllUserAccountsHandlerFunc(s.userAccountsHandler.GetAllUserAccounts)
+	// api.DeleteUserAccountHandler = operations.DeleteUserAccountHandlerFunc(s.userAccountsHandler.DeleteUserAccount)
 
 	// Init http server
 
-	listenAddress = "0.0.0.0"
-	listenPort = 8080
+	// listenAddress = "0.0.0.0"
+	// listenPort = 8080
 
 	s.server, err = server.New(api, listenAddress, listenPort)
 	if err != nil {
