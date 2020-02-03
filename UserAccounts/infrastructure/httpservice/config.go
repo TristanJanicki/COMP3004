da802b1877320844fa7903c13f79b7e3fee9e284
Tristan Janicki <tristan.janicki@gmail.com>
2020-02-03 14:14:30 -0500

    Update config.go

--- UserAccounts/infrastructure/httpservice/config.go
+++ UserAccounts/infrastructure/httpservice/config.go
@@ -68,7 +68,7 @@ var DefaultConfig = Config{
 var DefaultServiceConfig = ServiceConfig{
 	SqlDbHost:     "quantr.cii6qa7deotz.us-east-1.rds.amazonaws.com",
 	SqlDbPort:     3306,
-	SqlDbName:     "quantr",
+	SqlDbName:     "",
 	SqlDbUser:     "admin",
 	SqlDbPassword: "5efPemPEwZrBfhvQ",
 
