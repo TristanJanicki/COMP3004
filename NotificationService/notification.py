2932d689eea78d4ed100f8ba554ba85e11e5db73
Youssef <35567619+YoussefEljaji@users.noreply.github.com>
2020-02-09 19:26:04 -0500

    Update notification.py
    Added comments

--- NotificationService/notification.py
+++ NotificationService/notification.py
@@ -1,12 +1,17 @@
 from pyfcm import FCMNotification
-API_KEY="AIzaSyC-HslrXcXMhXfLY64wnW5WstvygE_Tf5Y" 
+API_KEY="AIzaSyC-HslrXcXMhXfLY64wnW5WstvygE_Tf5Y" #temp key for testing purposes
+#Push notification
+#input: users ID
+#output: a notification to the user
 def pushNotif(user_RegId):
 	push_service = FCMNotification(API_KEY)
 	
 	msgTit= "Quant.r"
 	msgBod = "A result on your tradeName has occured"
 	result= push_service.notify_single_device(registration_Id= user_RegID, message_title=msgTit, message_body=msgBody)
-	
+#multi push notification
+#input: user ID's
+#output: a notification sent to multiple users
 def pushMultNotif(users_RegId):
 	push_service = FCMNotification(API_KEY)
 	
