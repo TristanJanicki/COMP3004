a9a9d73d14e9e4a199e91d33675474d38aac073e
Tristan Janicki <tristan.janicki@gmail.com>
2020-02-19 19:19:57 -0500

    fixing sign up

--- QuantrLogin/app/src/main/java/com/example/quantrlogin/ui/login/SignUp.java
+++ QuantrLogin/app/src/main/java/com/example/quantrlogin/ui/login/SignUp.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.quantrlogin.R;
 import com.example.quantrlogin.ui.login.LoginViewModel;
@@ -21,6 +22,8 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.net.URL;
 
+import kotlin.Unit;
+import networking_handlers.SignUpHandler;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -31,8 +34,8 @@ import okhttp3.Response;
 
 public class SignUp extends AppCompatActivity {
     private Button button_SignUp;
+    private EditText email;
     //private OkHttpClient client = new OkHttpClient();
-    private String url = "http://ec2-18-212-29-33.compute-1.amazonaws.com:80/v1/users/signup";
     private String tag;
 
     @Override
@@ -41,6 +44,8 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         button_SignUp = findViewById(R.id.signUp2);
+        email = findViewById(R.id.email);
+        final String emailStr = email.getText().toString();
 
         //send POST Sign up request
         //after that works
@@ -49,34 +54,29 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                OkHttpClient client = new OkHttpClient().newBuilder()
-                        .build();
-                MediaType mediaType = MediaType.parse("application/json");
-                RequestBody body = RequestBody.create(mediaType, "{\n \" email \": \"farzalk@gmail.com\",    \"firstName\": \"Farzal\",    \"lastName\": \"Khan\"}");
-                Request request = new Request.Builder()
-                        .url(url)
-                        .method("POST", body)
-                        .addHeader("X-Request-ID", "5d09ec8a-6ef2-43ce-8b80-25339cd8d5c4")
-                        .addHeader("Content-Type", "application/json")
-                        .build();
+//                SignUpHandler sh = new SignUpHandler(emailStr, emailStr, emailStr, emailStr);
+                SignUpHandler sh = new SignUpHandler("tristan", "tristan.janicki@gmail.com", "Tristan", "Janicki");
+                sh.execute();
+//                // .enqueue(new Callback() {
+//                    @Override
+//                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    @Override
+//                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+//                        if (response.isSuccessful()) {
+//                            //Log.i(tag, response.body().string());
+//
+//                            Log.i(tag, response.body().string());
+//
+//                            openAuthorizationActivity();
+//                        }
+//                    }
+//                });
 
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                        e.printStackTrace();
-                    }
 
-                    @Override
-                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                        if (response.isSuccessful()) {
-                            //Log.i(tag, response.body().string());
 
-                            Log.i(tag, response.body().string());
-
-                            openAuthorizationActivity();
-                        }
-                    }
-                });
             }
         });
     }
