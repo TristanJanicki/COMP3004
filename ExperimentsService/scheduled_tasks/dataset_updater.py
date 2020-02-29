614406338fd2ec868f50b6938e9db14e65b35344
Youssef <youssefeljaji@cmail.carleton.ca>
2020-02-29 18:18:51 -0500

    Added stuff to datasetupdater.py

--- ExperimentsService/scheduled_tasks/dataset_updater.py
+++ ExperimentsService/scheduled_tasks/dataset_updater.py
@@ -2,6 +2,32 @@
 
 # use the dataset files to get the name of every nasdaq and S&P500 listed company
 # get all the api parameters from alpha vantage, i think this loop is missing some time intervals
+import requests
+import csv
+import time
+import sys
+sys.path.append('COMP3004/DataSerivce/')
+from technicals import getPriceData,getTechnicalData
+
+def getTickerData(filename,dataset):
+	f=open(filename,"r")
+	contents=f.readlines()
+
+	for l in range (len(contents)-2):
+		tickData=contents[l+1]
+		ticker=''
+		for x in range (len(tickData)):
+			if(tickData[x]=='|'):
+				break
+			else: 
+				ticker+=tickData[x]
+		dataset.append(ticker)
+
+def getTickersFromDatasets():
+	dataset=[]
+	getTickerData("nasdaqlisted.txt",dataset)
+	getTickerData("otherlisted.txt",dataset)
+	return dataset
 
 technicals = "SMA,EMA,WMA,DEMA,TEMA,TRIMA,KAMA,MAMA,VWAP,T3,MACD,MACDEXT,STOCH,STOCHF,RSI,STOCHRSI,WILLR,ADX,ADXR,APO,PPO,MOM,BOP,CCI,CMO,ROC,ROCR,AROON,AROONOSC,MFI,TRIX,ULTOSC,DX,MINUS_DI,PLUS_DI,MINUS_DM,PLUS_DM,BBANDS,MIDPOINT,MIDPRICE,SAR,TRANGE,ATR,NATR,AD,ADOSC,OBV,HT_TRENDLINE,HT_SINE,HT_TRENDMODE,HT_DCPERIOD,HT_DCPHASE,HT_PHASOR".split(",")
 tickers = getTickersFromDatasets()
@@ -14,7 +40,7 @@ time_intervals = ["5min"]
 time_series_types = ["INTRA_DAY" "DAILY", "DAILY_ADJUSTED", "WEEKLY", "WEEKLY_ADJUSTED", "MONTHLY"]
 
 for tpt in time_series_types:
-    for ticker in tickers:
+   for ticker in tickers:
         for s in series_types:
             for t in technicals:
                 for tp in time_periods:
