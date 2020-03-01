fc1bd482c01bd2010e168cc51c4e9716f8041230
Tristan Janicki <tristan.janicki@gmail.com>
2020-02-29 19:51:53 -0500

    get data

--- DataSerivce/scheduled_tasks/dataset_updater.py
+++ DataSerivce/scheduled_tasks/dataset_updater.py
@@ -25,29 +25,30 @@ def getTickerData(filename,dataset):
 
 def getTickersFromDatasets():
 	dataset=[]
-	getTickerData("nasdaqlisted.txt",dataset)
-	getTickerData("otherlisted.txt",dataset)
+	getTickerData("./datasets/nasdaqlisted.txt",dataset)
+	getTickerData("./datasets/otherlisted.txt",dataset)
 	return dataset
 
-technicals = "SMA,EMA,WMA,DEMA,TEMA,TRIMA,KAMA,MAMA,VWAP,T3,MACD,MACDEXT,STOCH,STOCHF,RSI,STOCHRSI,WILLR,ADX,ADXR,APO,PPO,MOM,BOP,CCI,CMO,ROC,ROCR,AROON,AROONOSC,MFI,TRIX,ULTOSC,DX,MINUS_DI,PLUS_DI,MINUS_DM,PLUS_DM,BBANDS,MIDPOINT,MIDPRICE,SAR,TRANGE,ATR,NATR,AD,ADOSC,OBV,HT_TRENDLINE,HT_SINE,HT_TRENDMODE,HT_DCPERIOD,HT_DCPHASE,HT_PHASOR".split(",")
+list_of_technicals = "SMA,EMA,WMA,DEMA,TEMA,TRIMA,KAMA,MAMA,VWAP,T3,MACD,MACDEXT,STOCH,STOCHF,RSI,STOCHRSI,WILLR,ADX,ADXR,APO,PPO,MOM,BOP,CCI,CMO,ROC,ROCR,AROON,AROONOSC,MFI,TRIX,ULTOSC,DX,MINUS_DI,PLUS_DI,MINUS_DM,PLUS_DM,BBANDS,MIDPOINT,MIDPRICE,SAR,TRANGE,ATR,NATR,AD,ADOSC,OBV,HT_TRENDLINE,HT_SINE,HT_TRENDMODE,HT_DCPERIOD,HT_DCPHASE,HT_PHASOR".split(",")
 tickers = getTickersFromDatasets()
 api_call_count = 0
 
 series_types = ["close", "open", "high", "low"]
-technicals = ["RSI"] # theres a huge list of technicals available at https://www.alphavantage.co/documentation/
-time_periods = ["14"]
-time_intervals = ["5min"]
+time_intervals = ["1min", "5min", "15min", "30min", "daily", "weekly", "monthly"]
+
+time_periods = ["5", "10", "15", "20", "100", "200"] # TODO: update this list to be continuous in some range
+
 time_series_types = ["INTRA_DAY" "DAILY", "DAILY_ADJUSTED", "WEEKLY", "WEEKLY_ADJUSTED", "MONTHLY"]
 # 5 per minute
 # 500 per day
 for tpt in time_series_types:
    for ticker in tickers:
         for s in series_types:
-            for t in technicals:
+            for t in list_of_technicals:
                 for tp in time_periods:
                     for i in time_intervals:
                         if api_call_count > 0 and api_call_count % 5 == 0:
                             time.sleep(60)
-                        getPriceData(ticker, tp, i,timer_series_type=tpt, series_type=s)
-                        getTechnicalData(t, ticker, tp, i, s)
+                        technicals.getPriceData(ticker, tp, i,timer_series_type=tpt, series_type=s)
+                        technicals.getTechnicalData(t, ticker, tp, i, s)
                         api_call_count += 1
