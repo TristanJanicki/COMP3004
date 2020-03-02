b792758929d711a0b92ad27353bdc3904297b818
Tristan Janicki <tristan.janicki@gmail.com>
2020-03-02 13:06:07 -0500

    stuff

--- ExperimentsService/swagger_server/__main__.py
+++ ExperimentsService/swagger_server/__main__.py
@@ -9,7 +9,6 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Experiments Service'})
-
     app.run(port=8080)
 
 
--- ExperimentsService/swagger_server/models/get_experiments_result.py
+++ ExperimentsService/swagger_server/models/get_experiments_result.py
@@ -7,8 +7,6 @@ from typing import List, Dict  # noqa: F401
 
 from swagger_server.models.base_model_ import Model
 from swagger_server import util
-from swagger_server.models.existing_threshold_experiment import ExistingThresholdExperiment
-from swagger_server.models.existing_correlation_experiment import ExistingCorrelationExperiment
 
 
 class GetExperimentsResult(Model):
