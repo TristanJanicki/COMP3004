1af6b8d9ab810f0f8f0e4723a85afddcc3ff795b
Tristan Janicki <tristan.janicki@gmail.com>
2020-03-20 01:32:38 -0400

    fixes

--- ExperimentsService/swagger_server/infrastructure/cron_tasks/register_with_cron.py
+++ ExperimentsService/swagger_server/infrastructure/cron_tasks/register_with_cron.py
@@ -1,9 +1,14 @@
 # Run this file to register the experiment update checker cron job
 
+import options
+import os
 from crontab import CronTab
+import time
+log_file = "~/options_updater_cron_log.txt"
+f = open(log_file, "w+")
+f.write("Experiments Service Cron Log For Options Data Updater " + time.asctime())
 
-cron = CronTab(user='ubuntu')
-job = cron.new(command='python check_for_updates.py')
+cron = CronTab(user=True, log=log_file)
+job = cron.new(command='/usr/bin/python3 check_for_updates.py >> ' + log_file)
 job.minute.every(1)
-
 cron.write()
