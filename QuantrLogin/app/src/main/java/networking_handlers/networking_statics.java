b059d9afc2582bce7765ba159bf6a9ad7cab9afe
FarzalK <farzalkhan@gmail.com>
2020-04-02 14:10:34 -0400

    fixed bottom nav workability

--- QuantrLogin/app/src/main/java/networking_handlers/networking_statics.java
+++ QuantrLogin/app/src/main/java/networking_handlers/networking_statics.java
@@ -2,7 +2,8 @@ package networking_handlers;
 
 public final class networking_statics {
     public final static String url = "http://ec2-3-92-179-53.compute-1.amazonaws.com:80";
-    public final static String experiments = "http://27cbd63b.ngrok.io"; //http://b4dd920f.ngrok.io
-    public final static String userAccounts = "http://f7934984.ngrok.io"; //http://783b83e2.ngrok.io
+    public final static String experiments = "http://8141ade2.ngrok.io/"; //http://b4dd920f.ngrok.io
+    public final static String userAccounts = "http://65e7edd7.ngrok.io/"; //http://783b83e2.ngrok.io
     public final static String tdaURL = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A%2F%2F8b8f0859.ngrok.io%2Fdata%2FTD%2Fcallback&client_id=JPGIHQGE5ZUUQAEVAKT6JDKWM8WAALL2%40AMER.OAUTHAP";
 }
+
