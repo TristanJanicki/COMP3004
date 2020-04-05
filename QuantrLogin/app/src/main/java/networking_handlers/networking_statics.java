d581094a54b1fb6f2d892d02e35006c82a1f1676
ams767 <60052838+ams767@users.noreply.github.com>
2020-04-04 22:57:02 -0400

    Update networking_statics.java

--- QuantrLogin/app/src/main/java/networking_handlers/networking_statics.java
+++ QuantrLogin/app/src/main/java/networking_handlers/networking_statics.java
@@ -2,8 +2,8 @@ package networking_handlers;
 
 public final class networking_statics {
     public final static String url = "http://ec2-3-92-179-53.compute-1.amazonaws.com:80";
-    public final static String experiments = "http://8141ade2.ngrok.io/"; //http://b4dd920f.ngrok.io
-    public final static String userAccounts = "http://65e7edd7.ngrok.io/"; //http://783b83e2.ngrok.io
+    public final static String experiments = "http://ec2-34-205-71-141.compute-1.amazonaws.com:8080"; //http://b4dd920f.ngrok.io
+    public final static String userAccounts = "http://ec2-52-207-108-209.compute-1.amazonaws.com:8090"; //http://783b83e2.ngrok.io
     public final static String tdaURL = "https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=http%3A%2F%2F8b8f0859.ngrok.io%2Fdata%2FTD%2Fcallback&client_id=JPGIHQGE5ZUUQAEVAKT6JDKWM8WAALL2%40AMER.OAUTHAP";
 }
 
